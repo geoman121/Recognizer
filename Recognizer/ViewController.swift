e16e6ff24c981ffaa723728dd93757beeefc6bfd
George Manayath <George@georges-macbook-pro.local>
2019-05-17 12:02:40 +0530

    RecognizerApp

--- Recognizer/ViewController.swift
+++ Recognizer/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+    
+    var imagePicker = UIImagePickerController()
+    let resnetModel = Resnet50()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        if let image = imageView.image{
+            processImage(image: image )
+        }
+        
     }
-
-
+    func processImage(image:UIImage)
+    {
+        if let model = try? VNCoreMLModel(for: self.resnetModel.model){
+            let request = VNCoreMLRequest(model: model,completionHandler: { (request, error) in
+                if let results = request.results as? [VNClassificationObservation]
+                {
+                    for classification in results{
+                        print("ID: \(classification.identifier) Confidence: \(classification.confidence)")
+                    }
+                  //  self.descriptionLabel.text = results.first?.identifier
+                    let decp = results.first?.identifier.components(separatedBy: ",")
+                    self.descriptionLabel.text = decp?[0]
+                    if let confidence = results.first?.confidence{
+                        let confval = (confidence * 100.0).rounded()
+                        self.confidenceLabel.text = "\(confval)%"
+                }
+                }
+            })
+          
+                if let imageData = image.pngData()
+                {
+                    let handler = VNImageRequestHandler(data:imageData, options: [:])
+                    try? handler.perform([request])
+                }
+            
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+ func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+        imageView.image = selectedImage
+            processImage(image: selectedImage )
+        
+    }
+    picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
